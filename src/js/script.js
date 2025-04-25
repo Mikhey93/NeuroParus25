032913595bae175f350ca76764983d015b93f16c
Mikhey93 <mikhey93@icloud.com>
2025-04-25 14:22:13 +0400

    feat: import scripts

--- src/js/script.js
+++ src/js/script.js
@@ -4,21 +4,25 @@ import accordion from "./modules/accordion.js";
 import slider from "./modules/slider.js"
 import tabs from "./modules/tabs.js";
 import fixedMenu from "./modules/scroll-menu.js";
-
-
+import scrollToTop from "./modules/scroll.js";
+import specialistsInfoModals from "./modules/specialists-info.js";
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
-
   // Mobile menu at all pages
   mobMenu('.humb', '.header__bottom-wrapper', 'humb_active', 'header__bottom-wrapper_active');
   // Modals at all pages
   modals();
   // Slider at therapy.html page
   slider('.reviews-therapy__slider', '.reviews-therapy__slider-img', '.arrow-left', '.arrow-right');
-  //Accordion 
+  // Accordion 
   accordion('.accordion-card', '.accordion-top', '.accordion-bottom', '.accordion-icon__img', 'accordion-icon__img_active', 'accordion-bottom_active');
   // Tabs at parents.html page
   tabs('.tabs__titles-wrapper', '.tab__title', '.specialists-parents__cards-wrapper', 'tab__title_active', 'flex');
+  // Fixed menu at all pages
   fixedMenu('.header__bottom-wrapper', 'header__bottom-wrapper_fixed');
+  // Pageup scroll at all pages
+  scrollToTop();
+  // Specialists info modals at parents.html page
+  specialistsInfoModals();
 });
